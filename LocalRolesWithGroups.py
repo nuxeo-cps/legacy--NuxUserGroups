6b1f408f650eea2a333f8f9facfe9f00bd8c72d8
sfermigier <devnull@localhost>
2003-07-14 21:34:39 +0000

    Cosmetic.

--- LocalRolesWithGroups.py
+++ LocalRolesWithGroups.py
@@ -1,6 +1,6 @@
-# (c) 2002 Nuxeo SARL <http://nuxeo.com>
-# (c) 2002 Florent Guillaume <mailto:fg@nuxeo.com>
-# (c) 2002 Préfecture du Bas-Rhin, France
+# Copyright (c) 2002 Nuxeo SARL <http://nuxeo.com>
+# Copyright (c) 2002 Préfecture du Bas-Rhin, France
+# Author: Florent Guillaume <mailto:fg@nuxeo.com>
 # See license info at the end of this file.
 # $Id$
 
@@ -14,7 +14,7 @@ __version__ = '$Revision$'[11:-2]
 import string, re, urllib, os
 
 from zLOG import LOG, INFO
-from Globals import InitializeClass, DTMLFile
+from Globals import DTMLFile
 from Acquisition import aq_base
 from AccessControl.Permissions import change_permissions
 from AccessControl.PermissionRole import PermissionRole
@@ -46,7 +46,7 @@ RoleManager.manage_editLocalGroupRoles__roles__ = PermissionRole(change_permissi
 
 # used by listLocalRoles
 def has_local_group_roles(self):
-    dict=self.__ac_local_group_roles__ or {}
+    dict = self.__ac_local_group_roles__ or {}
     return len(dict)
 RoleManager.has_local_group_roles = has_local_group_roles
 RoleManager.has_local_group_roles__roles__ = PermissionRole(change_permissions)
@@ -54,25 +54,27 @@ RoleManager.has_local_group_roles__roles__ = PermissionRole(change_permissions)
 
 # used by listLocalRoles
 def get_local_group_roles(self):
-    dict=self.__ac_local_group_roles__ or {}
-    keys=dict.keys()
+    dict = self.__ac_local_group_roles__ or {}
+    keys = dict.keys()
     keys.sort()
-    info=[]
+    info = []
     for key in keys:
-        value=tuple(dict[key])
+        value = tuple(dict[key])
         info.append((key, value))
     return tuple(info)
+
 RoleManager.get_local_group_roles = get_local_group_roles
 RoleManager.get_local_group_roles__roles__ = PermissionRole(change_permissions)
 
 
-# used where ?
+# XXX: used where ?
 def groups_with_local_role(self, role):
     got = {}
     for group, roles in self.get_local_group_roles():
         if role in roles:
             got[group] = None
     return got.keys()
+
 RoleManager.groups_with_local_role = groups_with_local_role
 RoleManager.groups_with_local_role__roles__ = PermissionRole(change_permissions)
 
@@ -92,6 +94,7 @@ def get_valid_groupids(self):
     keys = dict.keys()
     keys.sort()
     return tuple(keys)
+
 RoleManager.get_valid_groupids = get_valid_groupids
 RoleManager.get_valid_groupids__roles__ = PermissionRole(change_permissions)
 
@@ -100,6 +103,7 @@ RoleManager.get_valid_groupids__roles__ = PermissionRole(change_permissions)
 def get_local_roles_for_groupid(self, groupid):
     dict=self.__ac_local_group_roles__ or {}
     return tuple(dict.get(groupid, []))
+
 RoleManager.get_local_roles_for_groupid = get_local_roles_for_groupid
 RoleManager.get_local_roles_for_groupid__roles__ = PermissionRole(change_permissions)
 
@@ -108,16 +112,17 @@ def manage_addLocalGroupRoles(self, groupid, roles=[], REQUEST=None):
     """Add local group roles to a user."""
     if not roles:
         raise ValueError, 'One or more roles must be given!'
-    dict=self.__ac_local_group_roles__ or {}
+    dict = self.__ac_local_group_roles__ or {}
     local_group_roles = list(dict.get(groupid, []))
     for r in roles:
         if r not in local_group_roles:
             local_group_roles.append(r)
     dict[groupid] = local_group_roles
-    self.__ac_local_group_roles__=dict
+    self.__ac_local_group_roles__ = dict
     if REQUEST is not None:
-        stat='Your changes have been saved.'
+        stat = 'Your changes have been saved.'
         return self.manage_listLocalRoles(self, REQUEST, stat=stat)
+
 RoleManager.manage_addLocalGroupRoles = manage_addLocalGroupRoles
 RoleManager.manage_addLocalGroupRoles__roles__ = PermissionRole(change_permissions)
 
@@ -126,26 +131,28 @@ def manage_setLocalGroupRoles(self, groupid, roles=[], REQUEST=None):
     """Set local group roles for a user."""
     if not roles:
         raise ValueError, 'One or more roles must be given!'
-    dict=self.__ac_local_group_roles__ or {}
-    dict[groupid]=roles
-    self.__ac_local_group_roles__=dict
+    dict = self.__ac_local_group_roles__ or {}
+    dict[groupid] = roles
+    self.__ac_local_group_roles__ = dict
     if REQUEST is not None:
         stat='Your changes have been saved.'
         return self.manage_listLocalRoles(self, REQUEST, stat=stat)
+
 RoleManager.manage_setLocalGroupRoles = manage_setLocalGroupRoles
 RoleManager.manage_setLocalGroupRoles__roles__ = PermissionRole(change_permissions)
 
 
 def manage_delLocalGroupRoles(self, groupids, REQUEST=None):
     """Remove all local group roles for a user."""
-    dict=self.__ac_local_group_roles__ or {}
+    dict = self.__ac_local_group_roles__ or {}
     for groupid in groupids:
         if dict.has_key(groupid):
             del dict[groupid]
-    self.__ac_local_group_roles__=dict
+    self.__ac_local_group_roles__ = dict
     if REQUEST is not None:
         stat='Your changes have been saved.'
         return self.manage_listLocalRoles(self, REQUEST, stat=stat)
+
 RoleManager.manage_delLocalGroupRoles = manage_delLocalGroupRoles
 RoleManager.manage_delLocalGroupRoles__roles__ = PermissionRole(change_permissions)
 
